f06c80079d9ca54a5f4f06cd52e1341923647f6a
Vivek Kumar <vivek.kumar@infibeam.net>
2016-10-12 22:34:26 +0530

    Server static assests on heroku

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,7 @@ module BoardGame
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.serve_static_assets = true
     config.middleware.delete Rack::Lock
   end
 end
